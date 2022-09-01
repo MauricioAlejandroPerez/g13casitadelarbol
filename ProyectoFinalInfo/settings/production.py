6bb78ae2cb287e511af8356602ab026c61e08ca9
Mauricio Alejandro Pérez <map.j91@gmail.com>
2022-09-01 19:45:48 -0300

    asd

--- ProyectoFinalInfo/settings/production.py
+++ ProyectoFinalInfo/settings/production.py
@@ -23,7 +23,7 @@ DATABASES = {
     }
 }
 
-db_from_env= dj_database_url.config(conn_max_age=500)
+db_from_env= dj_database_url.config(conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 django_heroku.settings(locals())
 
