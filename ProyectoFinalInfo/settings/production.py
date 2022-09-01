e2d55be7725250289457759e77d5966e784aa2e0
Mauricio Alejandro Pérez <map.j91@gmail.com>
2022-09-01 19:52:41 -0300

    big changes

--- ProyectoFinalInfo/settings/production.py
+++ ProyectoFinalInfo/settings/production.py
@@ -23,8 +23,8 @@ DATABASES = {
     }
 }
 
-db_from_env= dj_database_url.config(conn_max_age=500, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+
+DATABASES['default']= dj_database_url.config(conn_max_age=500, ssl_require=True)
 django_heroku.settings(locals())
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
